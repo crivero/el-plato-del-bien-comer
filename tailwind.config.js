ce76cd5eeb45aee72d001a5d3f4f408060808993
criverog <2821824+crivero@users.noreply.github.com>
2025-04-22 21:43:14 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}",
-           "./components/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
-    safelist: [
+  safelist: [
     "bg-green-500",
     "bg-yellow-400",
     "bg-orange-400",
     "bg-red-500",
-    "bg-amber-900",
+    "bg-amber-900"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
