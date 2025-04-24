e9ccdf26546e59273821c171f216700a64dbbd22
criverog <2821824+crivero@users.noreply.github.com>
2025-04-24 14:09:36 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import JuegoAlimentos from './index';
+import JuegoAlimentos from './JuegoAlimentos';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
