79cf1e03676c48abbfe41c0fa4a8648397cb37ed
criverog <2821824+crivero@users.noreply.github.com>
2025-04-24 20:53:39 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.', // raíz del proyecto
   plugins: [react()],
   build: {
     outDir: 'dist',
